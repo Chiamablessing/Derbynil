02eb21ca379cfc3b8c5eccbcd390dc20fbb2f822
Somto <somtoprecious1@gmail.com>
2024-10-23 05:38:30 -0700

    Updated

---
+++ dribble/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Inspiration from './Components/Inspiration/Inspiration'
+import Secondpage from './Components/Secondpage/Secondpage'
+import Thirdpage from './Components/Thirdpage/Thirdpage'
+import Fourthpage from './Components/Fourthpage/Fourthpage'
+
+function App() {
+  return (
+    <div>
+      <Inspiration/>
+      <Secondpage/>
+      <Thirdpage/>
+      <Fourthpage/>
+    </div>
+  )
+}
+
+export default App
